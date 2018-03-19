74fcfc1509d573e8e3e5594b9c96c779c341ed61
akhan227 <akhan227@uic.edu>
2018-03-19 16:55:00 -0500

    second install

--- activities/a18-1/sw.js
+++ activities/a18-1/sw.js
@@ -1,6 +1,17 @@
-self.addEventListener('install', function(event) {
-  console.log("install", event);
-});
+// self.addEventListener('install', function(event) {
+//   console.log("install", event);
+// });
 self.addEventListener('activate', function(event) {
   console.log("activate", event);
 });
+
+self.addEventListener('install', function(e) {
+  console.log('Install',e);
+  e.waitUntil(
+    caches.open("cache-simple").then(function(cache) {
+      console.log('[ServiceWorker] Caching index.html');
+      return cache.add("index.html");
+    })
+  );
+});
+
