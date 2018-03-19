4e37cc6b9172b6566294c1cd877adb7a2b5201f0
akhan227 <akhan227@uic.edu>
2018-03-19 16:48:28 -0500

    activate

--- activities/a18-1/sw.js
+++ activities/a18-1/sw.js
@@ -1,3 +1,6 @@
 self.addEventListener('install', function(event) {
   console.log("install", event);
 });
+self.addEventListener('activate', function(event) {
+  console.log("activate", event);
+});
