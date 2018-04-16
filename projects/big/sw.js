37ffdf5771eda5ccba604dade72486266665a925
akhan227 <akhan227@uic.edu>
2018-04-16 15:16:04 -0500

    activate

--- projects/big/sw.js
+++ projects/big/sw.js
@@ -21,9 +21,24 @@ self.addEventListener('fetch', function(event) {
 					return response;
 				}
 				if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-				  return;
+					return;
 				}
 				return fetch(event.request);
 			})
 	);
+});
+
+self.addEventListener('activate', function(event) {
+	var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.map(function(cacheName) {
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				})
+				);
+		})
+		);
 });
