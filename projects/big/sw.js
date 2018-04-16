d6c8e124fd60293d454a23540fa9f600457df895
akhan227 <akhan227@uic.edu>
2018-04-16 14:19:00 -0500

    caching

--- projects/big/sw.js
+++ projects/big/sw.js
@@ -1,4 +1,17 @@
+var CACHE_NAME = 'myTransitApp-cache-v1';
+var urlsToCache = [
+  '/',
+  '/imgs/halsted.jpg',
+];
+
+
 self.addEventListener('install', function(event){
 	// Perform install steps
-	console.log("Stuff");
+	event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
