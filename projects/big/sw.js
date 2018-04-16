d8ddeea5c9487739551306a7c3321ca219c67481
akhan227 <akhan227@uic.edu>
2018-04-16 17:25:57 -0500

    fetch

--- projects/big/sw.js
+++ projects/big/sw.js
@@ -7,12 +7,16 @@ var urlsToCache = [
 self.addEventListener('install', function(event){
 	event.waitUntil(caches.open(CACHE_NAME).then(function(cache){
 		console.log('Opened cache');
+
 		return cache.addAll(urlsToCache);
 	})
 	);
 });
 
 self.addEventListener('fetch', function(event) {
+	if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') {
+	  return;
+	}
 	event.respondWith(
 		caches.match(event.request)
 			.then(function(response) {
